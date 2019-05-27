323f50f694a8800f831eb01ca790c6d19a856f6b
LW-Ricarido <liweicqbuaa@gmail.com>
2019-05-27 17:14:45 +0800

    fix bugs of re of baidu_link allow

--- patentSpider/patentSpider/spiders/cprsSpider.py
+++ patentSpider/patentSpider/spiders/cprsSpider.py
@@ -3,16 +3,16 @@ import scrapy
 import re
 from  scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from patentSpider.items import PatentspiderItem
+from ..items import PatentspiderItem
 from urllib.parse import urlparse, parse_qs
 
 
 class CpsrspiderSpider(CrawlSpider):
     name = 'cprsSpider'
-    start_urls = ['https://xueshu.baidu.com/s?wd=X%E5%B0%84%E7%BA%BF&pn=0&tn=SE_baiduxueshu_c1gjeupa&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&sc_hit=1&bcp=2&ie=utf-8&filter=sc_type%3D%7B5%7D']
+    start_urls = ['https://xueshu.baidu.com/s?wd=%E7%8A%81%E5%9C%B0&pn=0&tn=SE_baiduxueshu_c1gjeupa&ie=utf-8&filter=sc_type%3D%7B5%7D&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&bcp=2&sc_hit=1']
 
     #start_urls = ['https://xueshu.baidu.com/s?wd=%E6%9C%BA%E6%A2%B0&tn=SE_baiduxueshu_c1gjeupa&ie=utf-8&usm=1&filter=sc_type%3D%7B5%7D&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&bcp=2&sc_hit=1']
-    baidu_link = LinkExtractor(allow=("[\s\S]*.xueshu.baidu.com/s?wd[\s\S]*&pn=[\s\S]*"))
+    baidu_link = LinkExtractor(allow=("[\s\S]*.xueshu.baidu.com/s\?wd[\s\S]*&pn=[\s\S]*"))
     cprs_link = LinkExtractor(allow=("[\s\S]*.cprs.patentstar.com.cn[\s\S]*"))
 
     rules = {
@@ -23,17 +23,16 @@ class CpsrspiderSpider(CrawlSpider):
 
     def baidu_parse(self, response):
         patent_url = response.xpath("//div[@id='1']//div[@class='sc_allversion']//a[@title='cprs.patentstar.com.cn']//@href").extract()
+        next_pages = response.xpath("//*[@id='page']/a[i[@class='c-icon-pager-next']]/@href").extract()
+        if next_pages:
+            next_pages = response.urljoin(next_pages)
+            yield scrapy.Request(next_pages, callback=self.baidu_parse)
         if patent_url.__len__() > 0:
             ANE = patent_url[0].replace('\r\n','')
             ANE = re.findall('ANE=[\s\S]*',ANE)[0]
             ANE = ANE.replace('ANE=','')
-            print("ANE:",ANE)
+            # print("ANE:",ANE)
             yield scrapy.FormRequest(url='http://cprs.patentstar.com.cn/Search/GetPatentByAN',formdata={'ANE':ANE},callback=self.cprs_parse)
-        next_pages= response.xpath("//*[@id='page']/a[i[@class='c-icon-pager-next']]/@href").extract_first()
-        print("nxt:",next_pages)
-        if next_pages:
-            next_pages=response.urljoin(next_pages)
-            yield scrapy.Request(next_pages,callback=self.baidu_parse)
     def cprs_parse(self, response):
         item = PatentspiderItem()
         item['title']=response.text
