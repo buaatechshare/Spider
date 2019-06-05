1155fe00286756be511075d2b6f229dd62e40712
RightChris <958717262@qq.com>
2019-06-05 16:40:11 +0800

    finish file I/O

--- patentSpider/patentSpider/spiders/cprsSpider.py
+++ patentSpider/patentSpider/spiders/cprsSpider.py
@@ -15,16 +15,14 @@ class CpsrspiderSpider(CrawlSpider):
     # start_urls = ['https://xueshu.baidu.com/s?wd=%E7%8A%81%E5%9C%B0&pn=0&tn=SE_baiduxueshu_c1gjeupa&ie=utf-8&filter=sc_type%3D%7B5%7D&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&bcp=2&sc_hit=1']
     # queries=['IP','机械','X射线','数字电路','电气','器件','通信','光电子','图像','广播电视']
     # start_urls = ['https://xueshu.baidu.com/s?wd=%E6%9C%BA%E6%A2%B0&tn=SE_baiduxueshu_c1gjeupa&ie=utf-8&usm=1&filter=sc_type%3D%7B5%7D&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&bcp=2&sc_hit=1']
-    queries = ['IP','计算机专业','器件','通信','空间信息','汇编','程序设计','微机','办公自动化','集成电路',
-               '设备','计算机软件','技术','多媒体','安全','光','信息工程','计算机病毒','传感器','操作系统',
-               '应用软件','科技','软件测试','计算机技术','媒体','广域网','网络','局域网','Linux','信息技术',
-               '信息','一系列','数据结构','微电子','算法','Java','自动化','入侵','电子商务','硬件',
-               '信号','计算机控制','网络工程','数字电路','电脑','并行','单片机','软件','光电','信息处理',
-               '通信工程','TCP','通讯','计算机科学','自然语言','电子线路','广播电视','电气','集成系统','网络系统',
-               'IT','软件工程','电子信息','信息科学','智能科学','科学技术','光电子','图像','计算机网络','接口技术',
-               '信号处理','密码学','网络管理','电气工程','数据库','信息安全','功能化','信息管理','数据处理','微电子学',
-               '电子设备','高分子','材料','聚合','工艺','制备','聚酯','合成','化工','制备',
-               '催化']
+   # start_urls = ['https://xueshu.baidu.com/s?wd=%E7%8A%81%E5%9C%B0&pn=0&tn=SE_baiduxueshu_c1gjeupa&ie=utf-8&filter=sc_type%3D%7B5%7D&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&bcp=2&sc_hit=1']
+    # start_urls = ['https://xueshu.baidu.com/s?wd=%E6%9C%BA%E6%A2%B0&tn=SE_baiduxueshu_c1gjeupa&ie=utf-8&usm=1&filter=sc_type%3D%7B5%7D&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D&bcp=2&sc_hit=1']
+    
+    queries = []
+    with open("keyword.txt",'r', encoding='UTF-8') as f:
+        for line in f.readlines():
+            line=line.strip('\n')
+            queries.append(line)
     def start_requests(self):
         start_urls=[]
         for query in self.queries:
